55741e3359bb2be1e207ac65b8239211946bd2b0
Tejasree V <tejasree.vangapelli@gmail.com>
2020-04-02 20:08:36 -0700

    Initial Commit

---
+++ MessagingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessagingApp
+//
+//  Created by Apple on 02/04/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
